a4a838f95b20f5d833a44b25b21aa406faba7a21
CesarCR320 <cesarcamacho2235@gmail.com>
2025-03-24 23:25:59 -0600

    Carga

---
+++ Sistemas/db.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = "localhost";  
+$usuario = "root";       
+$contraseña = "";           
+$nombredb = "empresa_el_punto"; 
+
+// Crear conexión
+$conexion = new mysqli($servidor, $usuario, $contraseña, $nombredb);
+
+?>
