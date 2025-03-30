13ce153c38f8e1139d362f6940a0c9106ddaa870
VladisAleksander <vladis_alexander@hotmail.com>
2025-03-30 02:07:40 -0600

    Estandarización de encabezado y menú de navegación

---
+++ Soporte/view/Main/nav.php
@@ -0,0 +1,124 @@
+<nav class="side-menu">
+    <ul class="side-menu-list">
+
+        <!-- Principal -->
+        <li class="grey with-sub">
+            <a href="../Home/">
+                <i class="font-icon glyphicon glyphicon-send"></i>
+                <span class="lbl">Principal</span>
+            </a>
+        </li>
+
+        <!-- Menu de Mensajes -->
+        <li class="purple with-sub">
+            <span>
+                <i class="font-icon font-icon-comments active"></i>
+                <span class="lbl">Mensajes</span>
+            </span>
+            <ul>
+                <li><a href="messenger.html"><span class="lbl">Mensajería</span></a></li>
+                <li><a href="chat.html"><span class="lbl">Mensajes</span><span class="label label-custom label-pill label-danger">8</span></a></li>
+                <li><a href="chat-write.html"><span class="lbl">Escribir Mensaje</span></a></li>
+                <li><a href="chat-index.html"><span class="lbl">Seleccionar Usario</span></a></li>
+            </ul>
+        </li>
+
+        <!-- Correo -->
+        <li class="red">
+            <a href="mail.html">
+                <i class="font-icon glyphicon glyphicon-send"></i>
+                <span class="lbl">Mail</span>
+            </a>
+        </li>
+
+        <!-- Menu de Perfil -->
+        <li class="blue with-sub">
+            <span>
+                <i class="font-icon font-icon-user"></i>
+                <span class="lbl">Perfil</span>
+            </span>
+            <ul>
+                <li><a href="#"><span class="lbl">Opción 1</span></a></li>
+                <li><a href="#"><span class="lbl">Opción 2</span></a></li>
+            </ul>
+        </li>
+
+        <!-- Menu de Soporte -->
+        <li class="orange-red with-sub">
+            <span>
+                <i class="font-icon font-icon-help"></i>
+                <span class="lbl">Soporte</span>
+            </span>
+            <ul>
+                <li><a href="../NewTicket/"><span class="lbl">Nuevo Ticket</span></a></li>
+                <li><a href="../ConsultTicket/"><span class="lbl">Consultar Ticket</span></a></li>
+                <li><a href="documentation.html"><span class="lbl">Documentación</span></a></li>
+                <li><a href="faq.html"><span class="lbl">FAQ Simple</span></a></li>
+                <li><a href="faq-search.html"><span class="lbl">FAQ Búsqueda</span></a></li>
+            </ul>
+        </li>
+
+        <!-- Contactos -->
+        <li class="red">
+            <a href="contacts.html" class="label-right">
+                <i class="font-icon font-icon-contacts"></i>
+                <span class="lbl">Contactos</span>
+                <span class="label label-custom label-pill label-danger">69</span>
+            </a>
+        </li>
+
+        <!-- Menu Genérico -->
+        <li class="grey with-sub">
+            <span>
+                <span class="font-icon font-icon-burger"></span>
+                <span class="lbl">Menu Anidado</span>
+            </span>
+            <ul>
+                <li><a href="#"><span class="lbl">Level 1</span></a></li>
+                <li><a href="#"><span class="lbl">Level 1</span></a></li>
+                <li class="with-sub">
+                    <span>
+                        <span class="lbl">Level 2</span>
+                    </span>
+                    <ul>
+                        <li><a href="#"><span class="lbl">Level 2</span></a></li>
+                        <li><a href="#"><span class="lbl">Level 2</span></a></li>
+                        <li class="with-sub">
+                            <span>
+                                <span class="lbl">Level 3</span>
+                            </span>
+                            <ul>
+                                <li><a href="#"><span class="lbl">Level 3</span></a></li>
+                                <li><a href="#"><span class="lbl">Level 3</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <section>
+        <header class="side-menu-title">Enlaces importantes</header>
+        <ul class="side-menu-list">
+            <li>
+                <a href="#">
+                    <i class="tag-color green"></i>
+                    <span class="lbl">Sitio Principal</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="tag-color grey-blue"></i>
+                    <span class="lbl">Empleados</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="tag-color red"></i>
+                    <span class="lbl">Preguntas</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+</nav><!--.side-menu-->
--- Soporte/view/NewTicket/index.php
+++ Soporte/view/NewTicket/index.php
@@ -1,1 +1,19 @@
+<?php   require_once '../Main/head.php'; ?>
+        <title>Soporte</title>
+    </head>
+    <body class="with-side-menu">
 
+        <?php   require_once '../Main/header.php'; ?>
+
+        <div class="mobile-menu-left-overlay"></div>
+        
+        <?php   require_once '../Main/nav.php'; ?>
+
+        <div class="page-content">
+            <div class="container-fluid">
+                <h1>Nuevo Ticket</h1>
+            </div><!--.container-fluid-->
+        </div><!--.page-content-->
+        <?php   require_once '../Main/js.php'; ?>
+    </body>
+</html>
