a446d6644a96a5ee4985d58ee260cc48b92769dd
zacmer-sy <zacmer532@outlook.com>
2025-04-09 22:56:11 -0600

    m

---
+++ Roles/ver_roles.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'conexion_r.php';
+require_once 'header.php'; // Incluye los estilos
+
+$query = "SELECT id, nombre, descripcion FROM roles";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    while($rol = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($rol['id']) . "</td>";
+        echo "<td>" . htmlspecialchars($rol['nombre']) . "</td>";
+        echo "<td>" . htmlspecialchars($rol['descripcion']) . "</td>";
+        echo "<td class='actions'>";
+        echo "<a href='editar_roles.php?id=" . $rol['id'] . "' class='edit'>Editar</a>";
+        echo "<a href='eliminar_roles.php?id=" . $rol['id'] . "' class='delete'>Eliminar</a>";
+        echo "<a href='permisos_roles.php?id=" . $rol['id'] . "'>Permisos</a>";
+        echo "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='4'>No se encontraron roles</td></tr>";
+}
+$conn->close();
+?>
