125f6b598fc3d20561e1fb621596c82cc8cd5065
VladisAleksander <vladis_alexander@hotmail.com>
2025-04-21 23:33:55 -0600

    Creación de Modelo y Controlador de nuevo ticket

---
+++ Soporte/controller/ticket.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "../config/conexion.php";
+    require_once "../modelos/Ticket.php";
+    $ticket = new Ticket();
+    
+    if (isset($_GET["op"])) {
+        switch ($_GET["op"]) {
+            case "insert":
+                $ticket->insert_ticket();
+            break;
+        }
+    }
+?>
--- Soporte/modelos/Ticket.php
+++ Soporte/modelos/Ticket.php
@@ -3,9 +3,21 @@
         public function insert_ticket($t_num, $t_tit, $area_id, $emp_id, $t_phone, $cat_id, $scat_id, $niv_id, $est_id, $sest_id, $t_desc, $t_crea, $t_upd) {
             $conectar = parent::conexion();
             parent::set_names();
-            $sql = "SELECT * FROM categorias WHERE c_stat=1;";
+            $sql = "INSERT INTO tickets (t_num, t_tit, area_id, emp_id, t_phone, cat_id, scat_id, niv_id, est_id, sest_id, t_desc, t_crea, t_upd) VALUES (?, ?, ?, ?, ?, ?, ?, 4, 1, 1, ?, NOW(), NOW();";
             $sql = $conectar->prepare($sql);
-            
+            $sql -> bindValue(1, $t_num);
+            $sql -> bindValue(2, $t_tit);
+            $sql -> bindValue(3, $area_id);
+            $sql -> bindValue(4, $emp_id);
+            $sql -> bindValue(5, $t_phone);
+            $sql -> bindValue(6, $cat_id);
+            $sql -> bindValue(7, $scat_id);
+            $sql -> bindValue(8, $niv_id);
+            $sql -> bindValue(9, $est_id);
+            $sql -> bindValue(10, $sest_id);
+            $sql -> bindValue(11, $t_desc);
+            $sql -> bindValue(12, $t_crea);
+            $sql -> bindValue(13, $t_upd);
             $sql->execute();
             return $resultado = $sql->fetchAll();
         }
